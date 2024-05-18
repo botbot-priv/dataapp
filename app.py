4181ec8461f5a3a64255229c04314a69dd6ab8d8
botbot-priv <59530281+botbot-priv@users.noreply.github.com>
2024-05-18 20:00:29 +0000

    commit

--- app.py
+++ app.py
@@ -2,14 +2,12 @@ import streamlit as st
 from multiapp import MultiApp
 import Home
 import Page1
-import Page2
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", Home.app)
 app.add_app("Page 1", Page1.app)
-app.add_app("Page 2", Page2.app)
 
 # The main app
 app.run()
